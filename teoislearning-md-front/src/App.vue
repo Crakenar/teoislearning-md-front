4c100e07937d2575de9f163ff8490cd7b80dae0f
Teo Berguerre <teothou@hotmail.fr>
2022-07-10 22:02:22 +0200

    simple design + markdown preview + shitty auth

---
+++ teoislearning-md-front/src/App.vue
@@ -0,0 +1,143 @@
+<script setup lang="ts">
+import {onMounted, ref} from "vue";
+import router from "@/router";
+import {store} from "@/assets/Store";
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "@/components/HelloWorld.vue";
+
+
+const mockAccount = { username: "t", password: "t"}
+
+onMounted(() => {
+  if (!store.authenticated){
+    router.replace({ name: "login" });
+  }
+});
+</script>
+
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <HelloWorld msg="Welcome to teoislearning-md !" />
+
+      <nav>
+        <RouterLink v-if="store.authenticated" to="/login" v-on:click.native="store.logout()" replace>Logout</RouterLink>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink v-if="store.authenticated" to="/mdpreview">MD Editor</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+
+<!--  <div id="app">-->
+<!--    <div id="nav">-->
+<!--      <router-link v-if="authenticated" to="/login" v-on:click.native="logout()" replace>Logout</router-link>-->
+<!--    </div>-->
+<!--    <router-view @authenticated="setAuthenticated" />-->
+<!--  </div>-->
+</template>
+
+<style>
+@import "@/assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: white;
+}
+
+nav a.router-link-exact-active:hover {
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+    place-items: center;
+  }
+
+  #app {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 0 2rem;
+  }
+
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ teoislearning-md-front/src/views/MarkdownPreviewView.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <div>
+      <button @click="tipsActive = !tipsActive">Tips md</button>
+      <div v-if="tipsActive" class="tips" >
+        <h3>Tips pour ecrire un .md</h3> <br>
+        <ul>
+          <li># Pour les titres (# -> h1 ## -> h2)</li>
+          <li>br Pour saut de ligne</li>
+          <li>- Pour les . de listes</li>
+        </ul>
+      </div>
+    </div>
+    <div>
+      <span>.MD Editor</span>
+      <textarea id="" v-model="markdown" cols="30" name="" rows="10"></textarea>
+    </div>
+    <div>
+      <span>PREVIEW IN HTML</span>
+      <div id="preview" v-html="markdownToHTML"></div>
+    </div>
+
+    <div class="btn-type">
+      <form v-on:submit.prevent="sendMD">
+        <input v-model="form.type" type="radio" name="type" id="projects" value="projects">
+        <label for="projects">Projets</label>
+        <input v-model="form.type" type="radio" name="type" id="experiences" value="experiences">
+        <label for="experiences">Experiences</label>
+        <div>
+          <button>Submit</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {computed, ref} from "vue";
+import {marked} from 'marked';
+import axios from "axios";
+
+const tipsActive = ref<boolean>(false)
+const form = ref({
+  type: ''
+});
+
+const markdown = ref("# Remplissez cet espace avec un style type '.md'")
+const markdownToHTML = computed(() => {
+  return marked(markdown.value)
+})
+
+function sendMD() {
+  console.log(form.value)
+  // axios.post('/markdownTOHTML', form.value)
+  //     .then((res) => {
+  //       console.log(res)
+  //     }).catch((error) => {
+  //       console.log(error)
+  //     })
+}
+</script>
+
+<style scoped>
+div {
+  padding: 10px;
+}
+
+.tips {
+  background-color: white;
+  color: brown;
+}
+
+div > textarea {
+  width: 100%;
+  resize: none;
+}
+
+#preview {
+  background-color: white;
+  color: black;
+}
+
+.btn-type {
+  text-align: center;
+}
+
+form > label {
+  padding: 10px;
+}
+</style>
